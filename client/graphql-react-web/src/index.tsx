111442ad63f33cca37895a03c2367a9f86abb07b
killi8n <killi8n.dev@gmail.com>
2021-01-22 17:24:17 +0900

    feat: added @apollo/client

--- client/graphql-react-web/src/index.tsx
+++ client/graphql-react-web/src/index.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      query GetRates {
+        books {
+          title
+          author
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
+
+// eslint-disable-next-line no-console
+console.log(client.version);
 
 ReactDOM.render(
   <React.StrictMode>
