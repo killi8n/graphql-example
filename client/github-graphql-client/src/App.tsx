814f8ddd0731290bcb3aa7e19be88cb011af5b84
killi8n <killi8n.dev@gmail.com>
2021-02-07 13:45:34 +0900

    feat: installed graphql

--- client/github-graphql-client/src/App.tsx
+++ client/github-graphql-client/src/App.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { FC } from 'react'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 import './App.css'
 
-function App() {
-    return <div>Hello</div>
-}
+const client = new ApolloClient({
+    uri: 'https://api.github.com/graphql',
+    cache: new InMemoryCache(),
+})
+
+const App: FC = () => (
+    <ApolloProvider client={client}>
+        <div>Hello</div>
+    </ApolloProvider>
+)
 
 export default App
